7d3dcde55d49be7cfd2de19b93e595ee012f85bf
ShebangDog <e1n18088@oit.ac.jp>
2020-07-22 18:16:55 +0900

    add parser

---
+++ src/main/java/analyzer/Parser.java
@@ -0,0 +1,82 @@
+package analyzer;
+
+import type.Integer;
+import type.*;
+
+import java.util.Objects;
+
+public class Parser {
+
+    public static final Parser parser = new Parser();
+
+    private Parser() {
+    }
+
+    private Token token;
+
+    public T parse(Token token) throws Exception {
+        this.token = token;
+        return parse();
+    }
+
+    public T parse() throws Exception {
+        if (consume("(")) return list();
+        if (consume("'")) return quote();
+        if (token.getKind() == TokenKind.Number) return singedNumber();
+        if (token.getKind() == TokenKind.Symbol) return symbol();
+
+        throw new Exception("parser");
+    }
+
+    private T list() throws Exception {
+        if (token == null) throw new Exception("null");
+        if (consume(")")) return Nil.nil;
+
+        final var topCons = new Cons();
+        var cons = topCons;
+
+        while (true) {
+            cons.car = parse();
+
+            if (consume(")")) return topCons;
+            if (consume(".")) {
+                cons.cdr = parse();
+                return topCons;
+            }
+
+            cons.cdr = new Cons();
+            cons = (Cons) cons.cdr;
+        }
+    }
+
+    private T quote() {
+        this.token = this.token.getNextToken();
+        return Nil.nil;
+    }
+
+    private T singedNumber() {
+        final var value = java.lang.Integer.parseInt(this.token.getValue());
+        final var result = new Integer(value);
+
+        this.token = this.token.getNextToken();
+
+        return result;
+    }
+
+    private T symbol() {
+        final var symbolName = this.token.getValue();
+        final var result = Objects.equals(symbolName, "NIL") ? Nil.nil : Symbol.symbol(symbolName);
+
+        this.token = this.token.getNextToken();
+
+        return result;
+    }
+
+    private boolean consume(String sign) {
+        if (!Objects.equals(sign, this.token.getValue())) return false;
+
+        this.token = this.token.getNextToken();
+        return true;
+    }
+
+}
